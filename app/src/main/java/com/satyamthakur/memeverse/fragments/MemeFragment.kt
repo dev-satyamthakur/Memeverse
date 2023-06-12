89d9251f1c6922c69de2640fe00f43347997bb8d
Satyam Thakur <sat.thakur20@gmail.com>
2023-06-12 21:50:37 +0530

    Added infinite scroll

--- app/src/main/java/com/satyamthakur/memeverse/fragments/MemeFragment.kt
+++ app/src/main/java/com/satyamthakur/memeverse/fragments/MemeFragment.kt
@@ -2,17 +2,15 @@ package com.satyamthakur.memeverse.fragments
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.AbsListView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.satyamthakur.memeverse.R
 import com.satyamthakur.memeverse.Utils.MarginItemDecoration
 import com.satyamthakur.memeverse.adapters.MemeAdapter
 import com.satyamthakur.memeverse.api.RetrofitClient
-import com.satyamthakur.memeverse.databinding.ActivityMainBinding
 import com.satyamthakur.memeverse.databinding.FragmentMemeBinding
 import com.satyamthakur.memeverse.models.Meme
 import kotlinx.coroutines.Dispatchers
@@ -22,23 +20,53 @@ import kotlinx.coroutines.withContext
 
 class MemeFragment : Fragment(R.layout.fragment_meme) {
 
-    lateinit var memList: List<Meme>
+    var memList = mutableListOf<Meme>()
     lateinit var binding: FragmentMemeBinding
+
+    // for infinite scroll
+    var isLoading = false
+
+    // Adapter
+    lateinit var mAdapter: MemeAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentMemeBinding.bind(view)
 
-        binding.memeRecyclerView.layoutManager = LinearLayoutManager(context)
+        var layoutManager = LinearLayoutManager(context)
+        binding.memeRecyclerView.layoutManager = layoutManager
+
+        mAdapter = MemeAdapter(memList)
+
+        binding.memeRecyclerView.adapter = mAdapter
         // item decorator for even margins of items in recyclerview
         binding.memeRecyclerView.addItemDecoration(
             MarginItemDecoration(24)
         )
         getMemeNow()
 
+        binding.memeRecyclerView.addOnScrollListener(object: RecyclerView.OnScrollListener() {
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val visibleItemCount = layoutManager.childCount
+                val pastVisibleItem = layoutManager.findFirstCompletelyVisibleItemPosition()
+                val total = mAdapter.itemCount
+
+                if (!isLoading) {
+                    if (visibleItemCount + pastVisibleItem >= total) {
+                        getMemeNow()
+                    }
+                }
+            }
+        })
+
     }
 
-        private fun getMemeNow() {
+    private fun getMemeNow() {
+        isLoading = true
+
         GlobalScope.launch(Dispatchers.IO) {
             try {
                 val response = RetrofitClient.apiService.getMemes()
@@ -49,9 +77,12 @@ class MemeFragment : Fragment(R.layout.fragment_meme) {
 
                     // get back on UI thread to update recyclerView and adapters
                     withContext(Dispatchers.Main) {
-                       memList = memesres!!.memes
+                        memList.addAll(memesres!!.memes as MutableList<Meme>)
+
+                        mAdapter.notifyDataSetChanged()
+
                         binding.memeProgressBar.visibility = View.GONE
-                        binding.memeRecyclerView.adapter = MemeAdapter(memList)
+                        isLoading = false
                     }
 
                 } else {
